9b99e2a9a6e27c963e914441b37baaf22da0f9b6
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-01-04 02:03:21 +0300

    Time: 11 ms (46.72%), Space: 45.6 MB (44.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -4,15 +4,12 @@ class Solution {
         int[] havingDigits = new int[]{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] havingSymbols = new String[]{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int count = 0;
-        int leftOverFromDivide;
         while (count < havingDigits.length) {
-            leftOverFromDivide = num / havingDigits[count];
             if (num >= havingDigits[count]) {
-                leftOverFromDivide = num - havingDigits[count] * leftOverFromDivide;
                 for (int i = 0; i < num / havingDigits[count]; i++) {
                     result.append(havingSymbols[count]);
                 }
-                num = leftOverFromDivide;
+                num = num - havingDigits[count] * (num / havingDigits[count]);
             }
             count++;
         }

44a2ea9ad03d3333cacb62ff290406d306465198
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-03-30 23:30:16 +0300

    Time: 2 ms (87.97%), Space: 40.9 MB (82.25%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*' && dp[0][j - 2]) {
+                dp[0][j] = true;
+            }
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sChar = s.charAt(i - 1);
+                char pChar = p.charAt(j - 1);
+                if (sChar == pChar || pChar == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pChar == '*') {
+                    char pCharPrev = p.charAt(j - 2);
+                    if (sChar == pCharPrev || pCharPrev == '.') {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}

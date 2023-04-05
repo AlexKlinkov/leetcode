079310cd5dc4ce6d3f4338cc38cf16143195b1cc
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-04-05 22:57:16 +0300

    Time: 4 ms (5.79%), Space: 40.1 MB (89.50%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+    if (s.length() % 2 != 0) {
+        return false;
+    }
+    Stack<Character> stack = new Stack<>();
+    Map<Character, Character> map = new HashMap<>();
+    map.put('(', ')');
+    map.put('[', ']');
+    map.put('{', '}');
+    for (int i = 0; i < s.length(); i++) {
+        char c = s.charAt(i);
+        if (map.containsKey(c)) {
+            stack.push(c);
+        } else if (stack.isEmpty() || map.get(stack.pop()) != c) {
+            return false;
+        }
+    }
+    return stack.isEmpty();
+    }
+}

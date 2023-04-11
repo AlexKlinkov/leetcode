cafd969ea3e9b78781fcf5fe66468a5b3be76ef9
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-04-12 00:37:27 +0300

    Time: 16 ms (53.24%), Space: 42.5 MB (30.15%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -2,7 +2,7 @@ class Solution {
     public int threeSumClosest(int[] nums, int target) {
     Arrays.sort(nums);
     int n = nums.length;
-    int closestSum = Integer.MAX_VALUE / 2; // avoid integer overflow
+    int closestSum = nums[0] + nums[1] + nums[2]; // initialize to sum of first three elements
     for (int i = 0; i < n - 2; i++) {
         int left = i + 1;
         int right = n - 1;

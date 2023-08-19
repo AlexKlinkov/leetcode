23d703fd72589df28859ad64bbc71ff4f9a5e948
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-08-19 16:08:24 +0300

    Time: 0 ms (100.00%), Space: 40.8 MB (83.24%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
     int k = 0; // Initialize a counter for elements not equal to val
-
     // Iterate through the array
     for (int i = 0; i < nums.length; i++) {
         if (nums[i] != val) {
@@ -9,7 +8,6 @@ class Solution {
             k++; // Increment the count
         }
     }
-
     return k; // k represents the number of elements not equal to val
     }
 }

0ff31eef6d6999122b68917ee40539c1a72e42ee
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-12-06 20:45:30 +0300

    Time: 2837 ms (8.30%), Space: 41.6 MB (9.58%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if (n == 0 || x == 1.0) return 1.0;
+        if (x == -1 && n < 0) return 1.0;
+        if (x == -1.0) return -1.0;
+        if (n == 1) return x;
+        if (n == -1) return 1.0/x;
+        double answer = x;
+        int count = 1;
+        int extentByModule = n == Integer.MIN_VALUE ? Math.abs(n + 1) : Math.abs(n);
+        int limit = extentByModule % 2 == 0 ? extentByModule/2 : extentByModule/2 + 1;
+        while (count < limit) {
+            answer *= x;
+            count++;
+        }
+        answer = extentByModule % 2 == 0 ? answer * answer : answer * (answer/x);
+        return n > 0 ? answer : (x < 0 ? -1.0/answer : 1.0/answer);
+    }
+}

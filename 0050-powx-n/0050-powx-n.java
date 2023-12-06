e6990e86e9b6dd7b5eb3470c460a562e75583497
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2023-12-06 21:00:21 +0300

    Time: 2800 ms (8.30%), Space: 41 MB (32.43%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -6,12 +6,11 @@ class Solution {
         if (n == 1) return x;
         if (n == -1) return 1.0/x;
         double answer = x;
-        int count = 1;
         int extentByModule = n == Integer.MIN_VALUE ? Math.abs(n + 1) : Math.abs(n);
         int limit = extentByModule % 2 == 0 ? extentByModule/2 : extentByModule/2 + 1;
-        while (count < limit) {
+        while (limit > 1) {
             answer *= x;
-            count++;
+            limit--;
         }
         answer = extentByModule % 2 == 0 ? answer * answer : answer * (answer/x);
         return n > 0 ? answer : (x < 0 ? -1.0/answer : 1.0/answer);

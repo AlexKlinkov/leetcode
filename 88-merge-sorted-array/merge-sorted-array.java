320540698d895953caa0c0bf2ffe7e3f15884c4b
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2025-10-12 12:15:30 +0300

    Time: 10 ms (2.01%) | Memory: 42.3 MB (44.35%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+                int i = m - 1; // the last elem in nums1
+        int j = n - 1; // the last elem in nums2
+        int k = m + n - 1; // the last position in nums1
+
+        // We are walking from the end and fill up the largest elems
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i -= 1;
+            } else {
+                nums1[k] = nums2[j];
+                j -= 1;
+            }
+            k -= 1;
+        }
+        // if nums2 has rested elems, it means all of them are the smallest
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j -= 1;
+            k -= 1;
+        }
+        for (int l = 0; l < nums1.length; l++) {
+            System.out.println(nums1[l]);
+        }
+    }
+}

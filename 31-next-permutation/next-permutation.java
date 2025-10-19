00176352fdb408ef2152ce1872286b2b918a3d47
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2025-10-19 17:20:57 +0300

    Time: 4 ms (2.82%) | Memory: 43.4 MB (11.41%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,7 +1,6 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if (nums.length == 1) // check the edge case, when array nums has a length equal to 1
-            return;
+        if (nums.length == 1) return;
         // check the edge case, when numbers are already sorted by descent
         if (IntStream.range(0, nums.length - 1).allMatch(index -> nums[index] >= nums[index + 1])) {
             Arrays.sort(nums);
@@ -18,7 +17,7 @@ class Solution {
                 int savedValue = nums[i];
                 nums[i] = nums[j];
                 nums[j] = savedValue;
-                // 3. sort array from (i + 1) to the end
+                // 3. sort array (from right part to the end)
                 Arrays.sort(nums, i + 1, nums.length);
                 break; // do only one permutation
             }

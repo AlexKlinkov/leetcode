4c784e478ff4c0474fdfa9b20589296e6a52f197
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2025-10-26 15:50:36 +0300

    Time: 0 ms (100.00%) | Memory: 42.2 MB (67.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,23 +1,38 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][matrix[i].length - 1] >= target && matrix[i][0] <= target) {
-                if (matrix[i][0] == target) return true;
-                if (matrix[i][matrix[i].length - 1] == target) return true;
-                int leftBoundary = 0;
-                int rightBoundary = matrix[i].length - 1;
-                while (leftBoundary <= rightBoundary) {
-                    int mid = (leftBoundary + rightBoundary) / 2;
-                    if (matrix[i][mid] == target) return true;
-                    if (target > matrix[i][mid]) {
-                        leftBoundary = mid + 1;
-                    } else {
-                        rightBoundary = mid - 1;
-                    }
+        int[] necessaryInnerArray = getInnerLineAsArray(matrix, target);
+        if (necessaryInnerArray != null) {
+            if (necessaryInnerArray[0] == target) return true;
+            if (necessaryInnerArray[necessaryInnerArray.length - 1] == target) return true;
+            byte leftBoundary = 0;
+            byte rightBoundary = (byte) (necessaryInnerArray.length - 1);
+            while (leftBoundary <= rightBoundary) {
+                byte mid = (byte) ((leftBoundary + rightBoundary) / 2);
+                if (necessaryInnerArray[mid] == target) return true;
+                if (target > necessaryInnerArray[mid]) {
+                    leftBoundary = (byte) (mid + 1);
+                } else {
+                    rightBoundary = (byte) (mid - 1);
                 }
-                return false;
             }
+            return false;
         }
         return false;
+    }
+
+    private int[] getInnerLineAsArray(int[][] matrix, int target) {
+        byte leftBoundary = 0;
+        byte rightBoundary = (byte) (matrix.length - 1);
+        while (leftBoundary <= rightBoundary) {
+            byte mid = (byte) ((leftBoundary + rightBoundary) / 2);
+            if (matrix[mid][0] <= target && matrix[mid][matrix[mid].length - 1] >= target) {
+                return matrix[mid];
+            } else if (matrix[mid][0] < target) {
+                leftBoundary = (byte) (mid + 1);
+            } else {
+                rightBoundary = (byte) (mid - 1);
+            }
+        }
+        return null;
     }
 }

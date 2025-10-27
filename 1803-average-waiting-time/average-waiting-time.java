fc081fc6b28d361faeb0d8fdb1faa10e5a56cd2c
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2025-10-27 21:48:21 +0300

    Time: 3 ms (91.68%) | Memory: 73.1 MB (26.40%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -1,20 +1,18 @@
 class Solution {
     public double averageWaitingTime(int[][] customers) {
         double totalWaitingTime = 0;
-        int currentWaitingTime = 0;
+        int startTimeForCooking = 0;
 
-        for (int i = 0; i < customers.length; i++) {
-            int arrivalTime = customers[i][0];
-            int cookTime = customers[i][1];
+        for (int[] customer: customers) {
+            int arriveTime = customer[0];
+            int cookingTime = customer[1];
+            // when the chef idles or when the customer arrived
+            int startCookingTime = Math.max(startTimeForCooking, arriveTime);
+            int completionTime = startCookingTime + cookingTime;
+            totalWaitingTime += completionTime - arriveTime;
 
-            int startCooking = Math.max(currentWaitingTime, arrivalTime);
-            int completionTime = startCooking + cookTime;
-            totalWaitingTime += completionTime - arrivalTime;
-
-            currentWaitingTime = completionTime;
-
+            startTimeForCooking = completionTime; // refresh start cooking time
         }
-
         return totalWaitingTime / customers.length;
     }
 }

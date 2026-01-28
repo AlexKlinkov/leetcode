0503587fb8acf8dfdce8aa1171293f257b1f2cbe
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2026-01-28 18:15:45 +0300

    Time: 0 ms (100.00%) | Memory: 46.5 MB (89.89%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) // check a list contains more than 1 element
+            return false;
+
+        ListNode pointerSlow = head; // turtle
+        ListNode pointerFast = head.next; // hare
+
+        while (pointerSlow != pointerFast) {
+            if (pointerFast == null || pointerFast.next == null)
+                return false;
+            pointerSlow = pointerSlow.next; // move 1 step
+            pointerFast = pointerFast.next.next; // move 2 steps
+        }
+        return true;
+    }
+}

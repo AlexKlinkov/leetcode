f39ea9e1e24ab561699dbdc68be89250f7507245
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2026-03-06 21:22:22 +0300

    Time: 2 ms (16.18%) | Memory: 61.9 MB (86.31%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,27 +1,29 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
+
         int leftIndex = 0;
         int rightIndex = 1;
-        int currentMinLengthOfSubArray = Integer.MAX_VALUE;
-        int currentSum = nums[leftIndex]; // takes the first elem of a given array
+        int currentLength = Integer.MAX_VALUE; // 1. take a big number as a current minimum length of a subarray
+        int currentSum = nums[leftIndex]; // 2. take the first elem of a given array for checking condition ( >= target)
 
-        while(leftIndex < rightIndex){
+        while (leftIndex < rightIndex) { // 3. continue while the window is being saved
+            if (currentSum >= target) { // 4. check the current sum of a subarray on corresponding to a target value
 
-            if (currentSum >= target){
-                if (rightIndex - leftIndex < currentMinLengthOfSubArray) {
-                    currentMinLengthOfSubArray = rightIndex - leftIndex;
+                if (rightIndex - leftIndex < currentLength) {
+                    currentLength = rightIndex - leftIndex; // 5. refresh length of a subarray by new minimum value
                 }
-                currentSum -= nums[leftIndex++];
+                currentSum -= nums[leftIndex++]; // 6. shift cursor to the right (from left position to the right one)
+
             } else {
-                
+
                 if (rightIndex < nums.length) {
-                    currentSum += nums[rightIndex++];
+                    currentSum += nums[rightIndex++]; // 7. expand a sliding window to the right
                 } else {
-                    currentSum -= nums[leftIndex++];
+                    currentSum -= nums[leftIndex++]; // 8. shrink a sliding window (put away values from the left)
                 }
             }
         }
 
-        return currentMinLengthOfSubArray ==  Integer.MAX_VALUE ? 0 : currentMinLengthOfSubArray;
+        return currentLength == Integer.MAX_VALUE ? 0 : currentLength;
     }
 }

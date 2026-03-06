a9fd2fcd22395dff25663f4cef362fd77c9cacfc
Alex Klinkov <97181431+AlexKlinkov@users.noreply.github.com>
2026-03-06 20:45:32 +0300

    Time: 2 ms (16.18%) | Memory: 62.1 MB (70.69%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,16 +1,27 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int left = 0;
-        int currentSum = 0;
-        int minLength = Integer.MAX_VALUE;
-        for (int right = 0; right < nums.length; right++) {
-            currentSum += nums[right];
-            while (currentSum >= target) {
-                minLength = Math.min(minLength, right - left + 1);
-                currentSum -= nums[left];
-                left++;
+        int leftIndex = 0;
+        int rightIndex = 1;
+        int currentMinLengthOfSubArray = Integer.MAX_VALUE;
+        int currentSum = nums[leftIndex]; // takes the first elem of a given array
+
+        while(leftIndex < rightIndex){
+
+            if (currentSum >= target){
+                if (rightIndex - leftIndex < currentMinLengthOfSubArray) {
+                    currentMinLengthOfSubArray = rightIndex - leftIndex;
+                }
+                currentSum -= nums[leftIndex++];
+            } else {
+                
+                if (rightIndex < nums.length) {
+                    currentSum += nums[rightIndex++];
+                } else {
+                    currentSum -= nums[leftIndex++];
+                }
             }
         }
-        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+
+        return currentMinLengthOfSubArray ==  Integer.MAX_VALUE ? 0 : currentMinLengthOfSubArray;
     }
 }
